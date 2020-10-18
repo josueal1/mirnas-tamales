12beb86e929e9ff54ace6eec3d999210be55c4e3
Josue Lopez <galvez2j@gmail.com>
2020-10-17 22:40:20 -0700

    feat(main.py): Added /test route with test exceptions

--- main.py
+++ main.py
@@ -17,5 +17,21 @@ handle_exceptions(app)
 def index():
     return render_template("gallery.html")
 
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+	if request.method == "GET":
+		try:
+			raise bugsnag.notify(Exception('getting /test'))
+		except Exception as e:
+			print("the following exception was raise in program: ", e)
+
+		finally:
+			return render_template("gallery.html")
+
+	elif request.method == "POST":
+		raise Exception("no code for POST method in /test")
+		return "No method here"
+
 if __name__ == "__main__":
     app.run()
